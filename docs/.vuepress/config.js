4269857071d7c33ad151e04c1ba89e0741274fb3
zhangcongcong <zhangcongcong@aikucun.com>
2021-06-22 11:52:33 +0800

    change dist directory

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const themeConfig = require('./config/theme/')
 module.exports = {
   title: "congcong's blogs",
   description: '闲聊技术',
-  dest: 'public',
+  base: '/summercongcong.github.io/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
