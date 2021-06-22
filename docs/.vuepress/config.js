d763653f2d10ab19f6181fc7d29a059fa0e4a44a
zhangcongcong <zhangcongcong@aikucun.com>
2021-06-22 11:44:26 +0800

    init blogs theme

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+const themeConfig = require('./config/theme/')
+
+module.exports = {
+  title: "congcong's blogs",
+  description: '闲聊技术',
+  dest: 'public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  theme: 'reco',
+  themeConfig,
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: ['@vuepress/medium-zoom', 'flowchart'] 
+}  
